0965f33ea1208b5efe654cb56846a249b56d6ad4
rails <jyfalcon@gmail.com>
2014-01-18 23:10:10 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kuaizi::Application.config.secret_key_base = 'd4fb572b6554e40aad8f34323193d26fe290fd728e03013fdac8517854a145ae79d2f587b15b96beb46cd705fe50dea96b73e94b81ab3d417b121cde24ce38d0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kuaizi::Application.config.session_store :cookie_store, key: '_kuaizi_session'
